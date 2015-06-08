7605a18a3d8051858552d33324382ab92d23f0cd
Nicolas Jouanin <nico@beerfactory.org>
2015-06-08 22:11:24 +0200

    Move to coroutine

--- hbmqtt/broker/handlers/connect.py
+++ hbmqtt/broker/handlers/connect.py
@@ -2,12 +2,14 @@
 #
 # See the file license.txt for copying permission.
 
+import asyncio
 from hbmqtt.message import MQTTMessage, ConnectMessage
 
 class ConnectHandler:
     def __init__(self, broker):
         self._broker = broker
 
+    @asyncio.coroutine
     def handle(message: ConnectMessage) -> MQTTMessage:
         # TBD
         pass
