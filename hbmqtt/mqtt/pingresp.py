ad74ad6a71f97556d70ee7309e3100a6fefd8ef6
Nicolas Jouanin <nico@beerfactory.org>
2015-06-17 07:25:52 +0200

    Add Ping req/resp packet

---
+++ hbmqtt/mqtt/pingresp.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2015 Nicolas JOUANIN
+#
+# See the file license.txt for copying permission.
+from hbmqtt.mqtt.packet import MQTTPacket, MQTTFixedHeader, PacketType, MQTTVariableHeader, MQTTPayload
+from hbmqtt.errors import HBMQTTException
+
+class PingRespPacket(MQTTPacket):
+    VARIABLE_HEADER = None
+    PAYLOAD = None
+
+    def __init__(self, fixed: MQTTFixedHeader=None):
+        if fixed is None:
+            header = MQTTFixedHeader(PacketType.PINGREQ, 0x00)
+        else:
+            if fixed.packet_type is not PacketType.DISCONNECT:
+                raise HBMQTTException("Invalid fixed packet type %s for PingRespPacket init" % fixed.packet_type)
+            header = fixed
+        super().__init__(header)
+        self.variable_header = None
+        self.payload = None
