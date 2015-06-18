dd25134902afb3cbb74d34d194b55936c5aef760
Nicolas Jouanin <nico@beerfactory.org>
2015-06-18 17:51:06 +0200

    Fix packet type checking

--- hbmqtt/mqtt/pingresp.py
+++ hbmqtt/mqtt/pingresp.py
@@ -10,9 +10,9 @@ class PingRespPacket(MQTTPacket):
 
     def __init__(self, fixed: MQTTFixedHeader=None, variable_header=None, payload=None):
         if fixed is None:
-            header = MQTTFixedHeader(PacketType.PINGREQ, 0x00)
+            header = MQTTFixedHeader(PacketType.PINGRESP, 0x00)
         else:
-            if fixed.packet_type is not PacketType.DISCONNECT:
+            if fixed.packet_type is not PacketType.PINGRESP:
                 raise HBMQTTException("Invalid fixed packet type %s for PingRespPacket init" % fixed.packet_type)
             header = fixed
         super().__init__(header)
