b719a8283c61a7b9bec0ad6e5164d5fe27c8fbb5
nico <nico@beerfactory.org>
2015-07-02 14:02:10 +0200

    HBMQTT-6 Add build() class method for packet PUBACK, PUBREC and PUBREL.

--- hbmqtt/mqtt/pubrec.py
+++ hbmqtt/mqtt/pubrec.py
@@ -19,3 +19,9 @@ class PubrecPacket(MQTTPacket):
         super().__init__(header)
         self.variable_header = variable_header
         self.payload = None
+
+    @classmethod
+    def build(cls, packet_id: int):
+        v_header = PacketIdVariableHeader(packet_id)
+        packet = PubrecPacket(variable_header=v_header, payload=None)
+        return packet
