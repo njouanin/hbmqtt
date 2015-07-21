40e03197d8d745e4356c37fe44b5abfad74eedc4
Nicolas Jouanin <nico@beerfactory.org>
2015-07-21 21:41:46 +0200

    Update samples

--- samples/client_publish.py
+++ samples/client_publish.py
@@ -12,23 +12,31 @@ from hbmqtt.client import MQTTClient
 
 logger = logging.getLogger(__name__)
 
+config = {
+    'will': {
+        'topic': '/will/client',
+        'message': b'Dead or alive',
+        'qos': 0x01,
+        'retain': True
+    }
+}
+#C = MQTTClient(config=config)
 C = MQTTClient()
 
 @asyncio.coroutine
 def test_coro():
-    yield from C.connect(uri='mqtt://iot.eclipse.org:1883/', username=None, password=None)
+    yield from C.connect(uri='mqtt://test.mosquitto.org:1883/', username=None, password=None)
     tasks = [
         asyncio.async(C.publish('a/b', b'TEST MESSAGE WITH QOS_0')),
         asyncio.async(C.publish('a/b', b'TEST MESSAGE WITH QOS_1', qos=0x01)),
         asyncio.async(C.publish('a/b', b'TEST MESSAGE WITH QOS_2', qos=0x02)),
     ]
     yield from asyncio.wait(tasks)
-
     logger.info("messages published")
     yield from C.disconnect()
 
 
 if __name__ == '__main__':
     formatter = "[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s"
-    logging.basicConfig(level=logging.DEBUG, format=formatter)
+    logging.basicConfig(level=logging.INFO, format=formatter)
     asyncio.get_event_loop().run_until_complete(test_coro())
--- samples/client_subscribe.py
+++ samples/client_subscribe.py
@@ -16,17 +16,17 @@ logger = logging.getLogger(__name__)
 C = MQTTClient()
 
 @asyncio.coroutine
-def test_coro():
-    yield from C.connect(uri='mqtt://iot.eclipse.org:1883/', username=None, password=None)
+def uptime_coro():
+    yield from C.connect(uri='mqtt://test.mosquitto.org:1883/', username=None, password=None)
     # Subscribe to '$SYS/broker/uptime' with QOS=1
     yield from C.subscribe([
                  {'filter': '$SYS/broker/uptime', 'qos': 0x01},
+                 {'filter': '$SYS/broker/load/#', 'qos': 0x00},
              ])
     logger.info("Subscribed")
-    for i in range (1,10):
-        ret = yield from C.deliver_message()
-        data = ret.packet.payload.data
-        logger.info(str(data))
+    for i in range(1, 10):
+        inflight = yield from C.deliver_message()
+        print(inflight.packet.payload.data)
     yield from C.unsubscribe(['$SYS/broker/uptime'])
     logger.info("UnSubscribed")
     yield from C.disconnect()
@@ -35,4 +35,4 @@ def test_coro():
 if __name__ == '__main__':
     formatter = "[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=formatter)
-    asyncio.get_event_loop().run_until_complete(test_coro())
+    asyncio.get_event_loop().run_until_complete(uptime_coro())
