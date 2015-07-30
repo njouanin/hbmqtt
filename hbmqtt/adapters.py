3185747f0992aec4ac50ff1e3590e41464bdfe64
Nicolas Jouanin <nico@beerfactory.org>
2015-07-30 22:50:41 +0200

    Split read/write adapters

--- hbmqtt/adapters.py
+++ hbmqtt/adapters.py
@@ -2,63 +2,59 @@
 #
 # See the file license.txt for copying permission.
 import asyncio
+import io
 from websockets.protocol import WebSocketCommonProtocol
 from asyncio import StreamReader, StreamWriter
-from hbmqtt.mqtt.packet import MQTTFixedHeader
 
 
-class ProtocolAdapter:
+class ReaderAdapter:
     """
-    Base class for all network protocol adapter.
+    Base class for all network protocol reader adapter.
 
-    Protocol adapters are used to adapt read/write operations on the network depending on the protocol used
+    Reader adapters are used to adapt read operations on the network depending on the protocol used
     """
 
     @asyncio.coroutine
-    def read_packet(self) -> bytes:
+    def read(self, n=-1) -> bytes:
         """
-        Read a MQTT packet from the protocol.
-        :return: packet read as vytes data
-        """
-
-    @asyncio.coroutine
-    def write_packet(self, data: bytes):
-        """
-        Writes a MQTT packet bytes to the protocol implementation
-        :param data: Bytes data to write
-        :return:
-        """
-
-    @asyncio.coroutine
-    def close(self):
-        """
-        Close network connection
-        :return:
+        Read up to n bytes. If n is not provided, or set to -1, read until EOF and return all read bytes.
+        If the EOF was received and the internal buffer is empty, return an empty bytes object.
+        :return: packet read as bytes data
         """
 
 
-class WebSocketsAdapter(ProtocolAdapter):
+class WriterAdapter:
     """
-    WebSockets API protocol adapter
-    This adapter relies on WebSocketCommonProtocol to read/write to/from a WebSocket.
+    Base class for all network protocol writer adapter.
+
+    Writer adapters are used to adapt write operations on the network depending on the protocol used
+    """
+
+    def write(self, data):
+        """
+        write some data to the protocol layer
+        """
+
+    @asyncio.coroutine
+    def drain(self):
+        """
+        Let the write buffer of the underlying transport a chance to be flushed.
+        """
+
+
+class WebSocketsReader(ReaderAdapter):
+    """
+    WebSockets API reader adapter
+    This adapter relies on WebSocketCommonProtocol to read from a WebSocket.
     """
     def __init__(self, protocol: WebSocketCommonProtocol):
         self._protocol = protocol
-        self._message_buffer = b''
+        self._stream = io.BytesIO(b'')
 
     @asyncio.coroutine
-    def read_packet(self) -> bytes:
-        if len(self._message_buffer) < 5:
-            yield from self._feed_buffer()
-        fixed_header = MQTTFixedHeader.from_bytes(self._message_buffer)
-        packet_length = fixed_header.bytes_length + fixed_header.remaining_length
-        self._feed_buffer(fixed_header.bytes_length + fixed_header.remaining_length)
-
-        header_offset = fixed_header.bytes_length
-        packet_length = fixed_header.bytes_length + fixed_header.remaining_length
-        data = self._message_buffer[header_offset:packet_length]
-        self._message_buffer = self._message_buffer[packet_length:]
-        return fixed_header, data
+    def read(self, n=-1) -> bytes:
+        yield from self._feed_buffer(n)
+        return self._stream.read(n)
 
     @asyncio.coroutine
     def _feed_buffer(self, n=1):
@@ -66,45 +62,65 @@ class WebSocketsAdapter(ProtocolAdapter):
         Feed the data buffer by reading a Websocket message.
         :param n: if given, feed buffer until it contains at least n bytes
         """
-        while len(self._message_buffer) < n:
+        while len(self._stream.getbuffer()) < n:
             message = yield from self._protocol.recv()
+            if message is None:
+                break
             if not type(message, bytes):
                 raise TypeError("message must be bytes")
-            self._message_buffer += message
+            self._stream.getbuffer().append(message)
+
+
+class WebSocketsWriter(WriterAdapter):
+    """
+    WebSockets API writer adapter
+    This adapter relies on WebSocketCommonProtocol to read from a WebSocket.
+    """
+    def __init__(self, protocol: WebSocketCommonProtocol):
+        self._protocol = protocol
+        self._stream = io.BytesIO(b'')
+
+    def write(self, data):
+        """
+        write some data to the protocol layer
+        """
+        self._stream.write(data)
 
     @asyncio.coroutine
-    def write_packet(self, data: bytes):
-        yield from self._protocol.send(data)
+    def drain(self):
+        """
+        Let the write buffer of the underlying transport a chance to be flushed.
+        """
+        yield from self._protocol.send(self._stream.getbuffer())
+        self._stream = io.BytesIO(b'')
 
-    @asyncio.coroutine
-    def close(self):
-        yield from self._protocol.close()
 
-
-class StreamProtocolAdapter(ProtocolAdapter):
+class StreamProtocolReader(ReaderAdapter):
     """
     Asyncio Streams API protocol adapter
-    This adapter relies on StreamReader and StreamWriter to read/write to/from a TCP socket.
+    This adapter relies on StreamReader to read from a TCP socket.
+    Because API is very close, this class is trivial
     """
-    def __init__(self, reader: StreamReader, writer: StreamWriter):
+    def __init__(self, reader: StreamReader):
         self._reader = reader
+
+    @asyncio.coroutine
+    def read(self, n=-1) -> bytes:
+        return (yield from self._reader.read(n))
+
+
+class StreamWriterAdapter(WriterAdapter):
+    """
+    Asyncio Streams API protocol adapter
+    This adapter relies on StreamWriter to write to a TCP socket.
+    Because API is very close, this class is trivial
+    """
+    def __init__(self, writer: StreamWriter):
         self._writer = writer
 
-    @asyncio.coroutine
-    def read_packet(self, n=-1) -> bytes:
-        header_bytes = yield from self._reader.read(5)
-        fixed_header = MQTTFixedHeader.from_bytes(header_bytes)
-        data = yield from self._reader.read(fixed_header.remaining_length)
-        return fixed_header, data
+    def write(self, data):
+        self._writer.write(data)
 
     @asyncio.coroutine
-    def write_packet(self, data: bytes):
-        try:
-            self._writer.write(data)
-            yield from self._writer.drain()
-        except ConnectionResetError:
-            self.close()
-
-    @asyncio.coroutine
-    def close(self):
-        self._writer.close()
+    def drain(self):
+        yield from self._writer.drain()
