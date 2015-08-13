92d84410c50e504ab46fc0af819f5a9c42bc6c82
Nicolas Jouanin <nico@beerfactory.org>
2015-08-13 22:23:39 +0200

    Fix description formatting

--- setup.py
+++ setup.py
@@ -8,16 +8,18 @@ from hbmqtt.version import get_version
 setup(
     name="hbmqtt",
     version=get_version(),
-    description="HBMQTT - HomeBrew MQTT\nclient/brocker using Python 3.4 asyncio library",
+    description="MQTT client/brocker using Python 3.4 asyncio library",
     author="Nicolas Jouanin",
     author_email='nico@beerfactory.org',
     url="https://github.com/beerfactory/hbmqtt",
     license='MIT',
     packages=find_packages(exclude=['tests']),
+    platforms='all',
     install_requires=[
         'transitions==0.2.5',
         'blinker',
-        'websockets'],
+        'websockets'
+    ],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
