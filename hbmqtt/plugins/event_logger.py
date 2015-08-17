d2d1a9f975d011acb5dda58b4cf36a3550df0b98
Nicolas Jouanin <nico@beerfactory.org>
2015-08-17 21:54:53 +0200

    Change formating

--- hbmqtt/plugins/event_logger.py
+++ hbmqtt/plugins/event_logger.py
@@ -14,7 +14,7 @@ class EventLoggerPlugin:
 
     @asyncio.coroutine
     def log_event(self, event_name):
-        self.logger.info("### EVENT FIRED: '%s' ###" % event_name)
+        self.logger.info("### '%s' EVENT FIRED ###" % event_name.replace('old', ''))
 
     def __getattr__(self, name):
         if name.startswith("on_"):
