1860038e87a81793569d4da29477a6da611759aa
nico <nico@beerfactory.org>
2015-08-19 13:54:31 +0200

    TODO : Commit missing file

--- hbmqtt/plugins/__init__.py
+++ hbmqtt/plugins/__init__.py
@@ -3,4 +3,5 @@
 # See the file license.txt for copying permission.
 
 from .event_logger import EventLoggerPlugin
-from .packet_logger import PacketLoggerPlugin
+# TODO: File is missing
+# from .packet_logger import PacketLoggerPlugin
