d0e32610bd1f612b8a91c79e858d36beaf78c2bf
Nicolas Jouanin <nico@beerfactory.org>
2015-08-29 15:29:09 +0200

    Add file auth plugin

--- hbmqtt/plugins/authentication.py
+++ hbmqtt/plugins/authentication.py
@@ -2,9 +2,10 @@
 #
 # See the file license.txt for copying permission.
 import logging
+from passlib.apps import custom_app_context as pwd_context
 
 
-class AnonymousAuthPlugin:
+class BaseAuthPlugin:
     def __init__(self, context):
         self.context = context
         try:
@@ -13,12 +14,20 @@ class AnonymousAuthPlugin:
             self.context.logger.warn("'auth' section not found in context configuration")
 
     def authenticate(self, *args, **kwargs):
-        authenticated = False
         if not self.auth_config:
             # auth config section not found
             self.context.logger.warn("'auth' section not found in context configuration")
-            authenticated = False
-        else:
+            return False
+        return True
+
+
+class AnonymousAuthPlugin(BaseAuthPlugin):
+    def __init__(self, context):
+        super().__init__(context)
+
+    def authenticate(self, *args, **kwargs):
+        authenticated = super().authenticate(*args, **kwargs)
+        if authenticated:
             allow_anonymous = self.auth_config.get('allow-anonymous', True)  # allow anonymous by default
             if allow_anonymous:
                 authenticated = True
@@ -35,4 +44,42 @@ class AnonymousAuthPlugin:
                 except KeyError:
                     self.context.logger.warn("Session informations not available")
                     authenticated = False
+        return authenticated
+
+
+class FileAuthPlugin(BaseAuthPlugin):
+    def __init__(self, context):
+        super().__init__(context)
+        self._users = dict()
+        self._read_password_file()
+
+    def _read_password_file(self):
+        password_file = self.auth_config.get('password-file', None)
+        if password_file:
+            try:
+                with open(password_file) as f:
+                    self.context.logger.debug("Reading user database from %s" % password_file)
+                    for l in f:
+                        line = l.strip()
+                        if not line.startswith('#'):    # Allow comments in files
+                            (username, pwd_hash) = line.split(sep=":", maxsplit=3)
+                            if username:
+                                self._users[username] = pwd_hash
+                                self.context.logger.debug("user %s , hash=%s" % (username, pwd_hash))
+                self.context.logger.debug("%d user(s) read from file %s" % (len(self._users), password_file))
+            except FileNotFoundError:
+                self.context.logger.warn("Password file %s not found" % password_file)
+        else:
+            self.context.logger.debug("Configuration parameter 'password_file' not found")
+
+    def authenticate(self, *args, **kwargs):
+        authenticated = super().authenticate(*args, **kwargs)
+        if authenticated:
+            session = kwargs.get('session', None)
+            hash = self._users.get(session.username, None)
+            if not hash:
+                authenticated = False
+                self.context.logger.debug("User '%s' unknown" % session.username)
+            else:
+                authenticated = pwd_context.verify(session.password, hash)
         return authenticated
--- tests/plugins/test_authentication.py
+++ tests/plugins/test_authentication.py
@@ -1,1 +1,98 @@
-__author__ = 'nico'
+# Copyright (c) 2015 Nicolas JOUANIN
+#
+# See the file license.txt for copying permission.
+
+import unittest
+import logging
+import os
+from hbmqtt.plugins.manager import BaseContext
+from hbmqtt.plugins.authentication import AnonymousAuthPlugin, FileAuthPlugin
+from hbmqtt.session import Session
+
+formatter = "[%(asctime)s] %(name)s {%(filename)s:%(lineno)d} %(levelname)s - %(message)s"
+logging.basicConfig(level=logging.DEBUG, format=formatter)
+
+
+class TestAnonymousAuthPlugin(unittest.TestCase):
+    def test_allow_anonymous(self):
+        context = BaseContext()
+        context.logger = logging.getLogger(__name__)
+        context.config = {
+            'auth': {
+                'allow-anonymous': True
+            }
+        }
+        s = Session()
+        s.username = ""
+        auth_plugin = AnonymousAuthPlugin(context)
+        self.assertTrue(auth_plugin.authenticate())
+
+    def test_disallow_anonymous(self):
+        context = BaseContext()
+        context.logger = logging.getLogger(__name__)
+        context.config = {
+            'auth': {
+                'allow-anonymous': False
+            }
+        }
+        s = Session()
+        s.username = ""
+        auth_plugin = AnonymousAuthPlugin(context)
+        self.assertFalse(auth_plugin.authenticate(session=s))
+
+    def test_allow_nonanonymous(self):
+        context = BaseContext()
+        context.logger = logging.getLogger(__name__)
+        context.config = {
+            'auth': {
+                'allow-anonymous': False
+            }
+        }
+        s = Session()
+        s.username = "test"
+        auth_plugin = AnonymousAuthPlugin(context)
+        self.assertTrue(auth_plugin.authenticate(session=s))
+
+
+class TestFileAuthPlugin(unittest.TestCase):
+    def test_allow(self):
+        context = BaseContext()
+        context.logger = logging.getLogger(__name__)
+        context.config = {
+            'auth': {
+                'password-file': os.path.join(os.path.dirname(os.path.realpath(__file__)), "passwd")
+            }
+        }
+        s = Session()
+        s.username = "user"
+        s.password = "test"
+        auth_plugin = FileAuthPlugin(context)
+        self.assertTrue(auth_plugin.authenticate(session=s))
+
+    def test_wrong_password(self):
+        context = BaseContext()
+        context.logger = logging.getLogger(__name__)
+        context.config = {
+            'auth': {
+                'password-file': os.path.join(os.path.dirname(os.path.realpath(__file__)), "passwd")
+            }
+        }
+        s = Session()
+        s.username = "user"
+        s.password = "wrong password"
+        auth_plugin = FileAuthPlugin(context)
+        self.assertFalse(auth_plugin.authenticate(session=s))
+
+    def test_unknown_password(self):
+        context = BaseContext()
+        context.logger = logging.getLogger(__name__)
+        context.config = {
+            'auth': {
+                'password-file': os.path.join(os.path.dirname(os.path.realpath(__file__)), "passwd")
+            }
+        }
+        s = Session()
+        s.username = "some user"
+        s.password = "some password"
+        auth_plugin = FileAuthPlugin(context)
+        self.assertFalse(auth_plugin.authenticate(session=s))
